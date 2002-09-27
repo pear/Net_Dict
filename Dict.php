08407b2a9e54e5afb9e882b5c7de109b130b4c34
Chandrashekhar Bhosle <cnb@php.net>
2002-09-27 08:23:15 +0000

    Release 1.0 of Net_Dict added to CVS
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_Dict/trunk@97091 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Dict.php
@@ -0,0 +1,484 @@
+<?php
+
+// {{{ license 
+ 
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+//
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2002 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Chandrashekhar Bhosle <cnb@freedomink.org>                  |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+
+// }}}
+// {{{ includes   
+require_once 'PEAR.php';
+require_once 'Net/Socket.php';
+
+// }}}
+
+/**
+*
+* @author Chandrashekhar Bhosle <cnb@freedomink.org>
+* @package Net
+*/
+class Net_Dict {
+    
+  	// {{{ properties
+    /**
+    * Socket object
+    *
+    * @var object
+    */
+   	var $_socket;
+
+    /**
+    * Server Information
+    *
+    * @var string
+    */
+    var $servinfo;
+   
+   	// }}}
+    // {{{ Constructor
+    /**
+    * Constructor
+    */
+    function Net_Dict()
+    {
+    }
+    
+    // }}}
+    // {{{ define()  
+    /**
+    * Gets definitions for the specified word in the specified database.
+    *
+    * @param string $word
+    * @param string $database
+    * 
+    * @return mixed Array of definitions if sucessful, else PEAR_Error
+    */
+    function define($word, $database='*')
+    {
+        $resp = $this->_sendCmd("DEFINE $database $word");
+
+        if (PEAR::isError($resp)) {
+            return $resp;
+        } 
+        
+        list($num) = explode(' ', $resp['text'], 2);
+
+        for ($i = 0; $i < $num; $i++) {
+            $resp = $this->_socket->readLine();
+
+            preg_match("/(\d+?)\s+?\"(.+)?\"\s+?(\S+)\s+?\"(.+)?\"/", 
+                                                    $resp, $matches);
+
+            $defines[$i]['response']    = $resp;
+            $defines[$i]['word']        = $matches[2];
+            $defines[$i]['database']    = $matches[3];
+            $defines[$i]['description'] = $matches[4];
+
+            $resp = $this->_getMultiline();
+
+            $defines[$i]['definition'] = $resp['text'];
+        }
+   
+        return $defines;
+    }
+
+    // }}}
+    // {{{ match()
+    /**
+    * Searches an index for the dictionary, and reports words
+    * which were found using a particular strategy.
+    *
+    * @param string $word
+    * @param string $strategy
+    * @param string $database
+    *
+    * @return mixed Array of matches if successful, else PEAR_Error
+    */
+    function match($word, $strategy='substring', $database='*')
+    {
+        $resp = $this->_sendCmd("MATCH $database $strategy $word");
+
+        if (PEAR::isError($resp)) {
+            return $resp;
+        }
+
+        $resp = $this->_getMultiLine();
+        
+        $data = $resp['text'];
+
+        preg_match_all("/(\S+)?\s\"(.+?)\"/", $data, $matches);
+
+        for ($i = 0; $i < count($matches[0]); $i++) {
+            $matched[$i]['database'] = $matches[1][$i];
+            $matched[$i]['word']     = $matches[2][$i];
+        }
+
+        return $matched;
+    }
+    
+    // }}} 
+    // {{{ showDatabases()
+    /**
+    * Gets list of available databases
+    *
+    * @return mixed Array of databases if successful, else PEAR_Error
+    */
+    function showDatabases()
+    {
+        $resp = $this->_sendCmd('SHOW DB');
+    
+    	if (PEAR::isError($resp)) {
+            return $resp;
+        } 
+    
+        $resp = $this->_getMultiLine();
+    
+    	preg_match_all("/(\S+)?\s+?\"(.+?)\"/", $resp['data'], $matches);
+    
+    	for ($i = 0; $i < count($matches[0]); $i++) {
+    		$databases[$i]['database']    = $matches[1][$i];
+    		$databases[$i]['description'] = $matches[2][$i];
+    	}
+    
+    	return $databases;
+    }
+    
+    // }}} 
+     // {{{ showStrategies() 
+    /**
+    * Gets a list of available strategies
+    *
+    * @return mixed Array of strategies if successful, else PEAR_Error
+    */
+    function showStrategies()
+    {
+        $resp = $this->_sendCmd('SHOW STRAT');
+
+        if (PEAR::isError($resp)) {
+            return $resp;
+        }
+    
+        $resp = $this->_getMultiLine();
+
+        preg_match_all("/(\S+)?\s+?\"(.+?)\"/", $resp['data'], $matches);
+
+        for ($i = 0; $i < count($matches[0]); $i++) {
+            $strategies[$i]['strategy']    = $matches[1][$i];
+            $strategies[$i]['description'] = $matches[2][$i];
+        }
+
+        return $strategies;
+    }
+    
+    // }}}
+     // {{{ showInfo()
+    /**
+    * Gets source, copyright, and licensing information about the
+    * specified database.
+    *
+    * @param string $database
+    *
+    * @return mixed string if successful, else PEAR_Error
+    */
+    function showInfo($database)
+    {
+        return $this->simpleQuery('SHOW INFO ' . $database);
+    }
+    
+    // }}}  
+    // {{{ showServer()
+    /**
+    * Gets local server information written by the local administrator.
+    * This could include information about local databases or strategies,
+    * or administrative information such as who to contact for access to
+    * databases requiring authentication. 
+    *
+    * @return mixed string if sucessful, else PEAR_Error
+    */
+    function showServer()
+    {
+        return $this->simpleQuery('SHOW SERVER');
+    }
+    
+    // }}} 
+    // {{{ client()
+    /**
+    * Allows the client to provide information about itself
+    * for possible logging and statistical purposes.  All clients SHOULD
+    * send this command after connecting to the server.  All DICT servers
+    * MUST implement this command (note, though, that the server doesn't
+    * have to do anything with the information provided by the client).
+    *
+    * @param string $text
+    * 
+    * @return mixed string if successful, else PEAR_Error
+    */
+    function client($text)
+    {
+        $this->_sendCmd('CLIENT cnb');
+    }
+    
+    // }}}
+    // {{{ status()
+    /**
+    * Display some server-specific timing or debugging information.  This
+    * information may be useful in debugging or tuning a DICT server.  All
+    * DICT servers MUST implement this command (note, though, that the text
+    * part of the response is not specified and may be omitted).
+    *
+    * @return mixed string if successful, else PEAR_Error
+    */
+    function status()
+    {
+        $resp = $this->_sendCmd('STATUS');
+    
+        return $resp['text'];
+    }
+    
+    // }}} 
+    // {{{ help()
+    /** 
+    * Provides a short summary of commands that are understood by this
+    * implementation of the DICT server.  The help text will be presented
+    * as a textual response, terminated by a single period on a line by
+    * itself.  All DICT servers MUST implement this command.
+    *
+    * @return mixed string on success, else PEAR_Error
+    */
+    function help()
+    {
+        return $this->simpleQuery('HELP');
+    }
+    
+    // }}} 
+    // {{{ quit()
+    /**
+    * This command is used by the client to cleanly exit the server.
+    * All DICT servers MUST implement this command.
+    *
+    * @return mixed string on success, else PEAR_Error
+    */
+    function quit()
+    {
+            return $this->_sendCmd('QUIT');
+    } 
+    
+    // }}} 
+    // {{{ optionMIME()
+    /**
+    * Requests that all text responses be prefaced by a MIME header
+    * [RFC2045] followed by a single blank line (CRLF).
+    * 
+    * @return mixed
+    */
+    function optionMIME()
+    {
+    }
+    
+    // }}}
+    // {{{ auth()
+    /**
+    * The client can authenticate itself to the server using a username and
+    * password.  The authentication-string will be computed as in the APOP
+    * protocol discussed in [RFC1939].
+    *
+    * @param string $user
+    * @param string $auth
+    *
+    * @return mixed
+    */
+    function auth($user, $auth)
+    {
+    
+    }
+    
+    // }}}
+    // {{{ SASLAuth()
+    /**
+    * The Simple Authentication and Security Layer (SASL) is currently
+    * being developed [RFC2222].  The DICT protocol reserves the SASLAUTH
+    * and SASLRESP commands for this method of authentication.
+    *
+    * @param string $mechanism
+    * @param string $initial_response
+    *
+    * @return mixed
+    */
+    function SASLAuth($mechanism, $initial_response)
+    {
+    
+    }
+    
+    // }}}
+    // {{{ SASLResp()
+    /**
+    * The client will send all responses using the SASLRESP command and a
+    * BASE64-encoded parameter.
+    *
+    * @param string $response
+    *
+    * @return mixed
+    */
+    function SASLResp($response)
+    {
+    
+    }
+    
+    // }}}
+    // {{{  connect()
+    /**
+    * Connects to a dict server and sets up a socket
+    *
+    * @param string $server
+    *
+    * @return mixed true on success, else PEAR_Error
+    */
+    function connect($server='dict.org', $port='2628')
+    {
+    	$s = new Net_Socket;
+    
+    	$err = $s->connect($server, $port);
+    
+    	if (PEAR::isError($err)) {
+            return $err;
+        }
+    
+        $banner = $s->readLine(); 
+
+        preg_match("/\d{3} (.*) <(.*)> <(.*)>/", $banner, &$reg);
+        $this->servinfo["signature"] = $reg[1];
+        $this->servinfo["capabilities"] = explode(".", $reg[2]);
+        $this->servinfo["msg-id"] = $reg[3];
+
+        $this->_socket  = $s;
+
+        return true;
+    }
+    
+    // }}}
+    // {{{ _sendCmd()
+    /**
+    * Sends a command, checks the reponse, and
+    * if good returns the reponse, other wise
+    * returns false.
+    *
+    * @param  $cmd  Command to send (\r\n will be appended)
+    *
+    * @return mixed First line of response if successful, otherwise false
+    */
+    function _sendCmd($cmd)
+    {
+        $result = $this->_socket->writeLine($cmd);
+
+        if (PEAR::isError($result) AND $result) {
+            return $result;
+        }
+
+        $data = $this->_socket->readLine();
+
+        if (PEAR::isError($data)) {
+            return $data;
+        }
+
+        $resp['code'] = substr($data, 0, 3);
+        $resp['text'] = ltrim(substr($data, 3));
+
+        if (!Net_Dict::isOK($resp)) {
+            return new PEAR_Error( $resp['text'], 
+                                    $resp['code']);
+        }
+
+        return $resp;
+    }
+
+    // }}} 
+    // {{{ _getMultiline()
+    /**
+    * Reads a multiline reponse and returns the data
+    *
+    * @return mixed string on success or PEAR_Error
+    */
+    function _getMultiline()
+    {
+        $data = '';
+        while(($tmp = $this->readLine()) != '.') {
+            if (substr($tmp, 0, 2) == '..') {
+                $tmp = substr($tmp, 1);
+            }
+            $data .= $tmp . "\r\n";
+        }
+
+        $resp['text']   = substr($data, 0, -2);
+        $resp['status'] = $this->readLine();
+
+        return $resp;
+    }
+
+    // }}} 
+    // {{{ _readLine()
+    /**
+    * Alias to Net_Socket::readLine();
+    */
+    function readLine()
+    {
+        return $this->_socket->readLine();
+    }
+
+    // }}} 
+    // {{{ simpleQuery()
+    /**
+    * Runs a generic dict query
+    *
+    * @param string $query
+    * 
+    * @return mixed string on success, else PEAR_Error
+    */
+    function simpleQuery($query)
+    {
+        $resp = $this->_sendCmd($query);
+
+        if (PEAR::isError($resp)) {
+            return $resp;
+        }
+
+        $resp = $this->_getMultiLine();
+
+        return $resp['text'];
+    }
+
+    // }}}  
+    // {{{ isOK()
+    /**
+    * Checks if a response code is positive
+    * 
+    * @param array $resp
+    *
+    * @return boolean
+    */
+    function isOK($resp)
+    {
+        $positives = array(1, 2, 3);
+
+        return in_array(substr($resp['code'], 0, 1), $positives);
+    }
+
+    // }}} 
+
+} // end class Dict
---
+++ docs/dict.php
@@ -0,0 +1,121 @@
+#!/usr/local/bin/php -q
+<?php
+
+// {{{ includes
+require 'Console/Getopt.php';
+require '../Dict.php';
+// }}}
+// {{{ get options
+
+$longOptions  = array('help', 'databases', 'info=', 'server', 'status');
+
+$con = new Console_Getopt;
+
+$args = $con->readPHPArgv();
+
+// array_shift($args);
+
+$options = $con->getopt($args, '', $longOptions);
+
+if (PEAR::isError($options)) {
+	die($options->getMessage());
+}
+
+// }}}
+// {{{ help!
+function help()
+{
+echo <<<HELP
+
+Usage: dict [OPTIONS] keyword
+
+Options:
+	--help			Prints this help screen
+	--databases		Show DB list
+	--info=DATABASE		Show Info on DB
+	--server		Show Server Info	
+	--status		Show Status
+
+HELP;
+	exit(0);
+}
+
+if ('--help' == $options[0][0][0] or ( empty($options[0][0][0]) and empty($options[1][0]) )  ) help();
+
+// }}}
+// {{{ connect
+$d = new Net_Dict;
+
+$conn = $d->connect();
+
+if (PEAR::isError($conn)) {
+	die($conn->getMessage());
+}
+
+// }}}
+// {{{ define
+
+if (!empty($options[1][0])) {
+
+	foreach ($options[1] as $keyword) {
+	
+        	$defs = $d->define($keyword);
+
+        	if (PEAR::isError($defs)) {
+                	die($defs->getMessage());
+        	}
+
+        	foreach ($defs as $def) {
+                	echo $def['definition'];
+        	}
+	}
+}
+
+// }}}
+// {{{ options
+
+switch ($options[0][0][0]) {
+
+	case '--help':
+		help();
+		break;
+	case '--databases':
+
+		foreach ($d->showDatabases() as $db) 
+                	echo $db['database'] . ' : ' . $db['description']."\n";
+
+		break;
+
+	case '--info':
+
+		$info = $d->showInfo($options[0][0][1]);
+
+		if (PEAR::isError($info))
+			die($info->getMessage());
+
+		echo $info;
+
+		break;
+
+	case '--server':
+		
+		$server = $d->showServer();
+
+		if (PEAR::isError($server)) 
+			die($server->getMessage());
+
+		echo $server;
+
+		break;
+
+	case '--status':
+		
+		echo $d->status();
+
+		break;
+
+	default:
+		break;
+}
+
+// }}}
